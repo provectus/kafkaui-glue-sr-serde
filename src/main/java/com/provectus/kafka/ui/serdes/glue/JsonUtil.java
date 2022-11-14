c4d34b822caa62f007add57903ed6977a6c524a9
iliax <ikuramshin@provectus.com>
2022-11-14 18:53:54 +0400

    Serde.Serializer implemented

--- src/main/java/com/provectus/kafka/ui/serdes/glue/JsonUtil.java
+++ src/main/java/com/provectus/kafka/ui/serdes/glue/JsonUtil.java
@@ -3,12 +3,17 @@ package com.provectus.kafka.ui.serdes.glue;
 import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.io.JsonEncoder;
 
@@ -39,6 +44,25 @@ class JsonUtil {
     } catch (InvalidProtocolBufferException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  static Object avroFromJson(String json, Schema avroSchema) {
+    DatumReader<Object> reader = new GenericDatumReader<>(avroSchema);
+    try {
+      return reader.read(null, DecoderFactory.get().jsonDecoder(avroSchema, json));
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  static Object protoFromJson(String json, ProtobufSchema schema) {
+    DynamicMessage.Builder message = schema.newMessageBuilder();
+    try {
+      JsonFormat.parser().merge(json, message);
+    } catch (InvalidProtocolBufferException e) {
+      throw new RuntimeException(e);
+    }
+    return message.build();
   }
 
 }
